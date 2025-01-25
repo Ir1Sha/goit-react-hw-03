f3c4314f7586b3d88a90071bfcddf70e3a83a278
ginny <ir.gonccharova.up@gmail.com>
2025-01-25 18:29:05 +0200

    structure upd

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import Contact from '../Contact/Contact';
+import Contact from './ContactList.jsx';
 import styles from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
